8ae898db79f4e6c45e45859d96e20b084964e97f
alvarofdzg <alvarofdzg99@gmail.com>
2024-10-07 10:21:01 -0700

    Update 02_Setup

---
+++ 02_Setup/cuda_tester.cu
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello world" << endl;
+}
+
+// Run the command: nvcc 02_Setup/cuda_tester.cu -o 02_Setup/cuda_tester
+// Then, run "02_Setup/cuda_tester" to see "Hello world" in the CLI
